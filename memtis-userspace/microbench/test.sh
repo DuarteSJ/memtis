6a8c19e7dc84320097d63ec7872b447258c9b3c4
DuarteSJ <ddduarte@sapo.pt>
2026-05-26 17:28:31 +0100

    notes

--- memtis-userspace/microbench/test.sh
+++ memtis-userspace/microbench/test.sh
@@ -1,8 +1,12 @@
 # all on DRAM
-./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 0 -N 0 -S 46
+echo -e "\n===All on DRAM===\n"
+time ./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 0 -N 0 -S 46
 # pchase on Optane, seq on DRAM
-./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 2 -N 0 -S 46
+echo -e "\n===pchase on Optane, seq on DRAM===\n"
+time ./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 2 -N 0 -S 46
 # inverse
-./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 0 -N 2 -S 46
+echo -e "\n===pchase on DRAM, seq on Optane===\n"
+time ./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 0 -N 2 -S 46
 # all on Optane
-./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 2 -N 2 -S 46
+echo -e "\n===All on Optane===\n"
+time ./bench -R 0.5 -i 2 -A 2048 -B 2048 -r 2 -N 2 -S 46
